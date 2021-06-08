0ff32b8e106887050aabaa4b7d5fc954aa4f59e2
Josh Cooper <hi@joshc.uk>
2021-06-08 01:40:42 +0100

    Replaced nuxt link path from portfolio to blog.

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -3,7 +3,7 @@
     <h1> Blog! </h1>
 
     <li v-for="article in articles" :key="article.slug">
-      <nuxt-link :to="`/portfolio/${article.slug}`">
+      <nuxt-link :to="`/blog/${article.slug}`">
         {{ article.title }}
       </nuxt-link>
       <p>
