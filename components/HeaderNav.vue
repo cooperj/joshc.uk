f50dc41ed7ce8c4509a91aa972e525d585f012e0
Josh Cooper <hi@joshc.uk>
2021-10-20 16:56:02 +0100

    Added mobile nav bar scripting

--- components/HeaderNav.vue
+++ components/HeaderNav.vue
@@ -5,35 +5,61 @@
         Josh Cooper
       </nuxt-link>
     </span>
-    <div id="links" class="">
-      <a href="#" class="close-btn">
-        <i class="fas fa-times" />
-      </a>
+    <div id="links" :class="linksStateClass">
+        <header>
+          <span class="title">
+            <nuxt-link to="/">
+              Josh Cooper
+            </nuxt-link>
+          </span>
+          <a v-on:click="closeNav()" class="close-btn">
+            <i class="fas fa-times" />
+          </a>
+        </header>
       <ul>
         <li>
-          <nuxt-link to="/">
+          <nuxt-link to="/" v-on:click.native="closeNav()">
             About
           </nuxt-link>
         </li>
         <li>
-          <nuxt-link to="/portfolio/">
+          <nuxt-link to="/portfolio/" v-on:click.native="closeNav()">
             Portfolio
           </nuxt-link>
         </li>
         <li>
-          <nuxt-link to="/blog/">
+          <nuxt-link to="/blog/" v-on:click.native="closeNav()">
             Blog
           </nuxt-link>
         </li>
         <li>
-          <nuxt-link to="/contact/">
+          <nuxt-link to="/contact/" v-on:click.native="closeNav()">
             Contact
           </nuxt-link>
         </li>
       </ul>
     </div>
-    <a href="#" class="open-nav-btn">
+    <a v-on:click="openNav()" class="open-nav-btn">
       <i class="fas fa-bars" />
     </a>
   </header>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      linksStateClass: "links-closed"
+    }
+  },
+
+  methods: {
+    openNav: function () {
+      this.linksStateClass = "links-open"
+    },
+    closeNav: function () {
+      this.linksStateClass = "links-closed"
+    }
+  }
+}
+</script>
