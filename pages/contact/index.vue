25dd707aa80f9b206fe41b71e2baa3d208333b48
Josh Cooper <hi@joshc.uk>
2021-11-02 13:42:47 +0000

    Added twitter to contact page and rewrote copy

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -5,13 +5,17 @@
     </summary>
     <article>
       <p>
-        Hey <span aria-hidden="true">👋</span> Thank you for reaching out!
+        Hey <span aria-hidden="true">👋</span> If you would like a longer guaranteed response, please fill in the form below.
       </p>
       <p>
-        I would appreciate it if you could fill out the following form in as much detail as you can, so I can best deal with your query.
+        But, if you'd like to contact me via Twitter, albeit more informally, I'm <twitter username="joshcooperdev" />!
       </p>
       <p>
-        Please feel free to send 
+        I would appreciate it if you could fill out the form <span aria-hidden="true">📝</span> in as much detail as you can, so I can best deal with your query quickly <span aria-hidden="true">⏳</span>.
+      </p>
+      <p>
+        Please feel free to send things that you think I would like <span aria-hidden="true">🐙</span>, but don't add me to your mailing list.
+      </p>
       <div>
         <form action="/contact/submit/" name="contact" method="POST" netlify-honeypot="bot-field" data-netlify="true">
           <div class="hidden">
@@ -70,7 +74,9 @@
 </template>
 
 <script>
+import Twitter from '~/components/Twitter.vue'
 export default {
+  components: { Twitter },
   head () {
     return {
       title: 'Contact',
