6771ebf9a35b61caf25f11179b3c5e4128d4392a
Josh Cooper <hi@joshc.uk>
2024-09-14 17:13:09 +0100

    feat: boilerplate for tag pages

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,7 @@ import { transform } from "typescript";
 
 /** @type {import('tailwindcss').Config} */
 export default {
+  important: true,
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
